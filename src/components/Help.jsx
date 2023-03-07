57dd76ddd661c14dec19e5bd0ef8cb874067637d
Juan Pablo Gonzalez <juanpg@gmail.com>
2023-03-07 09:18:27 -0800

    Clarify help text

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -9,18 +9,19 @@ function Help() {
                 </div>
                 <div className="modal-body">
                     <h4>Goal</h4>
-                    <p>The objective of the game is to convert decimal numbers, from 1 to 255, to binary, as fast as possible, as many rounds as possible without mistake.</p>
+                    <p>The objective of the game is to convert decimal numbers from 1 to 255 to binary as fast as possible, for as many rounds as possible without making a mistake.</p>
                     <h4>Instructions</h4>
                     <ol>
                         <li>You are blue, the computer is red.</li>
                         <li>Click the 'Start' button to begin a new game.</li>
-                        <li>A random number from 1 to 255 will be display on the right. Use the mouse or the keyboard to toggle the blue zeros and ones until they display the binary equivalent of the decimal number.</li>
+                        <li>A random number from 1 to 255 will be displayed on the right. Use the mouse or the keyboard to toggle the blue zeros and ones, so they display the binary equivalent of the decimal number.</li>
                         <li>You can use the keyboard keys <b>a</b>, <b>s</b>, <b>d</b> and <b>f</b> to control bits 128, 64, 32 and 16, and keys <b>j</b>, <b>k</b>, <b>l</b> and <b>;</b> to control bits 8, 4, 2 and 1.</li>
-                        <li>Click the 'Submit' button, or press the Spacebar to confirm your choices and validate your answer.</li>
+                        <li>Click the 'Submit' button or press the Spacebar to confirm your choices. This will validate your answer.</li>
                         <li>Click the 'Start' button once again, or press the Spacebar, to begin a new round.</li>
                         <li>At rounds 21, 41, 61, 81 and 101 the game increases the level of difficulty as explained below, but the computer delay is reset to 2 seconds.</li>
                     </ol>
                     <h4>Difficulty levels</h4>
+                    <p>The game has five difficulty levels, which increase every 20 rounds. At each level, the computer delay is reset to 2s, and a new challenge is introduced:</p>
                     <ul>
                         <li>At level 21, the number to convert disappears after a few seconds.</li>
                         <li>At level 41, the bit values (128, 64, 32, etc.) disappear.</li>
