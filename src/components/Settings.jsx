78eb4122d195312e7b0805d57add6e948b58c3c1
Juan Pablo Gonzalez <juanpg@gmail.com>
2023-03-13 09:17:37 -0700

    Display error message on invalid mapping

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -5,6 +5,7 @@ import { useState, useRef, useEffect } from "react";
 function Settings({currentSettings, setCurrentSettings}) {
     const [activeIndex, setActiveIndex] = useState(0);
     const [mapping, setMapping] = useState(currentSettings.split(''));
+    const [valid, setValid] = useState(true);
     const inputRefs = useRef([]);
     const closeRef = useRef(null);
     const submitRef = useRef(null);
@@ -18,13 +19,16 @@ function Settings({currentSettings, setCurrentSettings}) {
         e.preventDefault();
         const s = new Set(mapping);
         if(s.size === 8) {
+            setValid(vl => true);
             setCurrentSettings(mapping.join(''));
             if(closeRef.current) {
                 closeRef.current.click();
-                setActiveIndex(0);
             }
         } else {
             // Show error
+            setValid(vl => false);
+            setActiveIndex(idx => 0);
+            inputRefs.current[0].focus();
         }
     }
 
@@ -39,10 +43,23 @@ function Settings({currentSettings, setCurrentSettings}) {
         }
     }, [activeIndex]);
 
+    useEffect(() => {
+        document.querySelector("#settingsDialog").addEventListener('show.bs.modal', (event) => {
+            setMapping(mp => currentSettings.split(''));
+            setValid(vl => true);
+        });
+        document.querySelector("#settingsDialog").addEventListener('shown.bs.modal', (event) => {
+            console.log('shown');
+            setActiveIndex(idx => 0);
+            inputRefs.current[0].focus();
+        })
+    }, [currentSettings]);
+
     const handleKeyDown = (event) => {
         event.preventDefault();
         switch (event.key) {
         case "Backspace":
+            onControlChange(' ');
             if (activeIndex > 0) {
                 setActiveIndex(activeIndex - 1);
             }
@@ -52,7 +69,7 @@ function Settings({currentSettings, setCurrentSettings}) {
             if (control.trim().length === 1 && /^[a-zA-Z0-9[\]/\\();,.'`´^²&é"+ç¡ñ\-=è_çà]*$/.test(control)) {
                 onControlChange(control);
                 if (activeIndex < inputRefs.current.length - 1) {
-                    setActiveIndex(activeIndex + 1);
+                    setActiveIndex(idx => activeIndex + 1);
                 } else {
                     submitRef.current.focus();
                 }
@@ -106,7 +123,21 @@ function Settings({currentSettings, setCurrentSettings}) {
                             </div>
                             <div className="row mb-3 settings">
                                 {renderInputs()}
-                            </div>            
+                            </div>
+                            {!valid && 
+                                <div className="row mb-3">
+                                    <div className="col-md">
+                                        <div className="alert alert-danger d-flex align-items-center" role="alert">
+                                            <svg style={{width: '1em', height: '1em', fill: 'currentColor'}} xmlns="http://www.w3.org/2000/svg" className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                            </svg>
+                                            <div>
+                                                Invalid combination set. Please try again.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            }
                             <div className="row mb-3">
                                 <div className="col-md d-flex gap-3 justify-content-center">
                                     <button ref={submitRef} type="Submit" className="btn btn-primary w-25" onClick={onSubmit}>Save</button>
